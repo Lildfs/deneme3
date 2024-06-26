a7853067b5aa487e5313d618eb301bb9623cca7a
Lildfs <leodepo02@outlook.com>
2024-06-26 11:00:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -25,14 +25,20 @@ cors_proxy.createServer({
   originWhitelist: originWhitelist,
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
-  removeHeaders: [
+   removeHeaders: [
     'cookie',
     'cookie2',
+    // Strip Heroku-specific headers
+    'x-request-start',
+    'x-request-id',
     'via',
     'connect-time',
-    'total-route-time'
-]
-,
+    'total-route-time',
+    // Other Heroku added debug headers
+    // 'x-forwarded-for',
+    // 'x-forwarded-proto',
+    // 'x-forwarded-port',
+  ],
   redirectSameOrigin: true,
   httpProxyOptions: {
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
